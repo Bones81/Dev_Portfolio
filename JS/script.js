9e88de101913fe4fa985fec5198a5d716777b2f8
Nathan Freeman <NathanLFreeman@gmail.com>
2021-08-06 21:35:51 -0400

    Update layout/styles for much of the page. Add grid and shadows to projects section. Fix footer to take up remaining space by turning body into flex container and putting footer at flex-grow: 1. Stuff like that.

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,26 @@
 const navProjectsEl = document.getElementById("nav-projects-el");
 const navBioEl = document.getElementById("nav-bio-el");
 const navContactEl = document.getElementById("nav-contact-el");
+const contentDefaultEl = document.getElementById("content-default");
 const projectsDiv = document.getElementById("projects");
 const bioDiv = document.getElementById("bio");
 const contactDiv = document.getElementById("contact");
 
 navProjectsEl.addEventListener("click", function() {
   //show Projects div
+  contentDefaultEl.classList.add('hidden');
   projectsDiv.classList.remove('hidden');
   bioDiv.classList.add('hidden');
   contactDiv.classList.add('hidden'); 
 });
 navBioEl.addEventListener("click", function() {
+  contentDefaultEl.classList.add('hidden');
   projectsDiv.classList.add('hidden');
   bioDiv.classList.remove('hidden');
   contactDiv.classList.add('hidden'); 
 });
 navContactEl.addEventListener("click", function() {
+  contentDefaultEl.classList.add('hidden');
   projectsDiv.classList.add('hidden');
   bioDiv.classList.add('hidden');
   contactDiv.classList.remove('hidden'); 
